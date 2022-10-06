8b0365a656fb0654c7dfd8690a9c5f85d8b807ee
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-10-05 23:13:43 -0400

    Finished StartMenu.dart Refactor

--- app_movil/lib/StartMenu.dart
+++ app_movil/lib/StartMenu.dart
@@ -19,100 +19,75 @@ class _StartMenu extends State<StartMenu> {
     );
 
     return Scaffold(
-      body: Container(
-        /*
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [
-                0.4,
-                0.7,
-                1
-              ],
-              colors: [
-                Color(0xfff3ede0),
-                Color(0xffCEA660),
-                Color(0xff6B7A40)
-              ]
-          )
-        ),
-         */
-
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 100
-              ),
-              child: Center(
-                child: Text.rich(
-                  TextSpan(
-                    children: <TextSpan> [
-                      TextSpan(text: "Bienvenido a ", style: TextStyle(fontSize: 30)),
-                      TextSpan(text: "Mip", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-                      TextSpan(text: "!", style: TextStyle(fontSize: 30)),
-                    ]
-                  )
-                ),
-              ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text.rich(
+              TextSpan(
+                children: <TextSpan> [
+                  TextSpan(text: "Bienvenido a ", style: TextStyle(fontSize: 30)),
+                  TextSpan(text: "Mip", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                  TextSpan(text: "!", style: TextStyle(fontSize: 30)),
+                ]
+              )
             ),
+          ),
 
-            SizedBox(
-              height: 50,
+          SizedBox(
+            height: 50,
+          ),
+
+          Center(
+            child: Container(
+                width: 150,
+                height: 150,
+                /*decoration: BoxDecoration(
+                   // color: Colors.red,
+                    borderRadius: BorderRadius.circular(50.0)),*/
+                child: Image(
+                  image: AssetImage('assets/icono.png'),
+                )
             ),
+          ),
 
-            Center(
-              child: Container(
-                  width: 150,
-                  height: 150,
-                  /*decoration: BoxDecoration(
-                     // color: Colors.red,
-                      borderRadius: BorderRadius.circular(50.0)),*/
-                  child: Image(
-                    image: AssetImage('assets/icono.png'),
-                  )
+          SizedBox(
+            height: 50,
+          ),
+
+          Container(
+            width: 250,
+            child: ElevatedButton(
+              style: elevButtonStyle,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+              },
+              child: Text(
+                'Inicia sesión',
+                style: TextStyle(color: Colors.white, fontSize: 15),
               ),
             ),
+          ),
 
-            SizedBox(
-              height: 50,
-            ),
+          SizedBox(
+            height: 5,
+          ),
 
-            Container(
-              width: 250,
-              child: ElevatedButton(
+          Container(
+            width: 250,
+            child: ElevatedButton(
                 style: elevButtonStyle,
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => RoleMenu()));
                 },
                 child: Text(
-                  'Inicia sesión',
+                  'Regístrate',
                   style: TextStyle(color: Colors.white, fontSize: 15),
                 ),
-              ),
             ),
+          ),
 
-            SizedBox(
-              height: 5,
-            ),
-
-            Container(
-              width: 250,
-              child: ElevatedButton(
-                  style: elevButtonStyle,
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => RoleMenu()));
-                  },
-                  child: Text(
-                    'Regístrate',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
-                  ),
-              ),
-            ),
-
-          ],
-        ),
+        ],
       ),
     );
   }
--- app_movil/lib/main.dart
+++ app_movil/lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login',
+      title: 'Mip',
+      theme: ThemeData(
+        primaryColor: Colors.black
+      ),
       debugShowCheckedModeBanner: false,
       home: StartMenu(),
     );
