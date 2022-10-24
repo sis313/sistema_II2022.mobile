2f57514f42cd1fcd2ff402e779c835bb86c409f6
PepinilloPB <edgar.carrasco@ucb.edu.bo>
2022-10-24 13:49:50 -0400

    Cambios DTO negocio y sucursal

--- app_movil/lib/DTO/Sucursal.dart
+++ app_movil/lib/DTO/Sucursal.dart
@@ -1,9 +1,9 @@
 class Sucursal{
   int id_branch;
-  int id_zone;
-  int id_location;
+  int id_zone = 1;
+  int id_location = 1;
   int id_business;
-  String address = "descr";
+  String address;
   DateTime open_hour;
   DateTime close_hour;
   String attention_days;
@@ -13,6 +13,11 @@ class Sucursal{
 
   Sucursal(this.id_branch, this.name, this.address);
 
-  /*Sucursal(this.id_branch, this.id_zone, this.id_location, this.id_business,
+  /*Sucursal(this.id_branch, this.id_business, this.address, this.open_hour,
+      this.close_hour, this.attention_days);*/
+
+/*Sucursal(this.id_branch, this.id_zone, this.id_location, this.id_business,
       this.address, this.open_hour, this.close_hour, this.attention_days);*/
+
+
 }
