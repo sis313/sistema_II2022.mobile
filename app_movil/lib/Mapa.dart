41c44931eb377ae15e8dc3538e45d9263911cabe
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2022-10-23 21:23:42 -0400

    add geolocator

--- app_movil/lib/Mapa.dart
+++ app_movil/lib/Mapa.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 import 'DTO/Negocio.dart';
@@ -15,6 +16,24 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
 
+  var user_position ;
+  bool showUser = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print("$position.latitude, $position.longitude ");
+    //var user = [position.latitude, $position.longitude];
+    setState(() {
+      user_position  = position;
+      showUser = true;
+    });
+  }
+
   //Datos de prueba para listado
   var negocios = [
     Negocio(1, 'Nombre Negocio 1', [Sucursal(1, 'Sucursal 1', ''), Sucursal(2, 'Sucursal 2', '')]),
@@ -27,6 +46,16 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
+
+    final elevButtonStyle = ElevatedButton.styleFrom(
+      primary: Colors.blue,
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      elevation: 10,
+      shape: new RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(30.0),
+      ),
+    );
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -44,7 +73,21 @@ class MapSampleState extends State<MapSample> {
                       source: 'OpenStreetMap contributors',
                       onSourceTapped: null,
                     ),
-                  ],
+                    Align(
+                    alignment: Alignment(0.9, 0.9),
+                    child:
+                    ElevatedButton(
+                      child: const Icon(
+                        Icons.adjust_sharp,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      style: elevButtonStyle,
+                      onPressed: () {
+                        getCurrentLocation();
+                      },),
+                  )],
+
                   children: [
                     TileLayer(
                       urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -52,20 +95,32 @@ class MapSampleState extends State<MapSample> {
                     ),
                     MarkerLayer(
                       markers: [
+                        //getMarkerUser(showUser, user_position),
+                      showUser != false ?
+                        Marker(
+                        point: LatLng(user_position.latitude, user_position.longitude),
+                          builder: (context) => Icon(
+                          Icons.person_pin,
+                          color: Colors.red,
+                          size: 50,
+                          )
+                        ):
                         Marker(
                             point: LatLng(-16.493730639624058, -68.13252864400924),
                             builder: (context) => Icon(
                               Icons.pin_drop_rounded,
                               color: Colors.red,
-                              size: 30,
+                              size: 50,
                             )
                         ),
+
                         //Plaza Abaroa -16.509119931820113, -68.12710156327141
                         Marker(
                             point: LatLng(-16.509119931820113, -68.12710156327141),
                             builder: (context) => IconButton(
                                 onPressed: (){
-                                  print("Boton Abaroa");
+                                 // getCurrentLocation();
+                                  //print();
                                   setState(() {
                                     showDialog(
                                         context: context,
@@ -143,7 +198,7 @@ class MapSampleState extends State<MapSample> {
                                   );
                                 });
                               },
-                              child: Icon(Icons.pin_drop, size: 30,)
+                              child: Icon(Icons.pin_drop, size: 50,)
                           ),
                         )
                       ],
@@ -181,4 +236,6 @@ class MapSampleState extends State<MapSample> {
         }
     );
   }
+
+
 }
