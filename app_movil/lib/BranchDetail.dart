90dbca1de09c555696ee1c0224c477bc4b9d662b
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-11-16 23:49:42 -0400

    create branch funtion integrated correctly

--- app_movil/lib/BranchDetail.dart
+++ app_movil/lib/BranchDetail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:provider/provider.dart';
 
 import 'DTO/Branch.dart';
@@ -16,7 +18,22 @@ class _BranchDetailState extends State<BranchDetail> {
   int idBusiness;
   _BranchDetailState(this.idBusiness);
 
+  // Provider
   Future<List<Branch>> branches;
+
+  // Crete Branch 1
+  TimeOfDay openTime = TimeOfDay.now();
+  TimeOfDay closeTime = TimeOfDay.now();
+  String valueAtencion;
+  var vectorAtencion = ['Lunes a Viernes', 'Lunes a Sabado', 'Toda la semana'];
+
+  // Create Branch 2
+  final controllerSucursal = TextEditingController();
+  final ctrollerNegocioName = TextEditingController();
+  final ctrollerNegocioDesc = TextEditingController();
+  String zona = "";
+  String municipio = "";
+  String ciudad = "";
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +42,7 @@ class _BranchDetailState extends State<BranchDetail> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Branch Info"),
+        backgroundColor: Color(0xffa7d676),
       ),
       body: FutureBuilder(
         future: branches,
@@ -41,7 +59,17 @@ class _BranchDetailState extends State<BranchDetail> {
             child: CircularProgressIndicator(),
           );
         },
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xffa7d676),
+        child: IconButton(
+            onPressed: (){
+              anadirSucursal_1();
+            },
+            icon: Icon(Icons.add),
+            tooltip: "Crear nueva sucursal",
+        ),
+      ),
     );
   }
 
@@ -83,6 +111,270 @@ class _BranchDetailState extends State<BranchDetail> {
           }
         });
       },
+    );
+  }
+
+  /// ***************** Alerts para CRUD *****************
+  anadirSucursal_1(){
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AlertDialog(
+                title: const Text('Nueva Sucursal'),
+                content: StatefulBuilder(
+                  builder: (BuildContext c, StateSetter setState){
+                    return Column(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Abre a las : ${openTime.hour}:${openTime.minute}",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                const SizedBox(width: 16),
+                                ElevatedButton(
+                                  onPressed: () async {
+                                    TimeOfDay newTime = openTime;
+                                    newTime = await showTimePicker(
+                                        context: context,
+                                        initialTime: openTime
+                                    );
+
+                                    if(newTime == null) return;
+
+                                    setState(() {
+                                      openTime = newTime;
+
+                                      if(openTime.hour >= closeTime.hour)
+                                        closeTime = openTime;
+                                    });
+                                  },
+                                  child: Text("Cambiar"),
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all(Color(0xfffbc78d)
+                                      ),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(18.0),
+                                              side: BorderSide(color: Color(0xfffbc78d))
+                                          )
+                                      )),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Cierra a las : ${closeTime.hour}:${closeTime.minute}",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                                const SizedBox(width: 16),
+                                ElevatedButton(
+                                  onPressed: () async {
+                                    TimeOfDay newTime = closeTime;
+                                    newTime = await showTimePicker(
+                                        context: context,
+                                        initialTime: closeTime
+                                    );
+                                    setState(() {
+                                      if(newTime == null ||
+                                          (openTime.hour == newTime.hour &&
+                                              openTime.minute > newTime.minute) ||
+                                          openTime.hour > newTime.hour)
+                                        closeTime = openTime;
+                                      else
+                                        closeTime = newTime;
+                                    });
+                                  },
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all(Color(0xfffbc78d)
+                                      ),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(18.0),
+                                              side: BorderSide(color: Color(0xfffbc78d))
+                                          )
+                                      )),
+                                  child: Text("Cambiar"),
+
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        DropdownButton<String>(
+                            value: valueAtencion,
+                            items: vectorAtencion.map(
+                                    (String e) =>
+                                    DropdownMenuItem<String>(
+                                        value: e,
+                                        child: Text(e)
+                                    )
+                            ).toList(),
+                            onChanged: (String value) {
+                              setState(() {
+                                this.valueAtencion = value;
+                              });
+                            }
+                        )
+                      ],
+                    );
+                  },
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancelar',style: TextStyle(color: Colors.white, fontSize: 15)),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Color(0xffef5a68)
+                        ),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Color(0xffef5a68))
+                            )
+                        )),
+                  ),
+                  TextButton(
+                      onPressed: () {
+
+                      },
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Color(0xffa7d676)
+                            ),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Color(0xffa7d676))
+                                )
+                            )),
+                        onPressed: () {
+                          anadirSucursal_2();
+                        },
+                        child: Text(
+                          'Continuar',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      )
+                  ),
+                ],
+              )
+            ],
+          );
+        }
+    );
+  }
+
+  anadirSucursal_2(){
+    GlobalKey<FormState> formkey = GlobalKey<FormState>();
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AlertDialog(
+                title: const Text('Nueva Sucursal'),
+                content: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  key: formkey,
+                  child: StatefulBuilder(
+                    builder: (BuildContext c, StateSetter setState){
+                      return Column(
+                        children: [
+                          TextFormField(
+                            validator: MultiValidator([
+                              RequiredValidator(
+                                  errorText: "Campo Requerido"
+                              ),
+                            ]),
+                            controller: controllerSucursal,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Direccion',
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextButton(
+                            child: Text("Buscar",
+                                style: TextStyle(color: Colors.white, fontSize: 15)),
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all(Color(0xfff9e2ae)
+                                ),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(18.0),
+                                        side: BorderSide(color: Color(0xfff9e2ae))
+                                    )
+                                )),
+
+                            onPressed: () async {
+                              var addresses = await Geocoder.local.findAddressesFromQuery(controllerSucursal.text);
+
+                              var first = addresses.first;
+                              setState(() {
+                                zona = first.subLocality;
+                                municipio = first.adminArea;
+                                ciudad = first.locality;
+
+                                print("Direccion: ${first.addressLine}");
+                                print("AdminArea: ${first.adminArea}");//Municipio
+                                print("CountryName: ${first.countryName}");
+                                print("FeatureName: ${first.featureName}");
+                                print("Localidad: ${first.locality}"); //Ciudad
+                                print("SubAdminArea: ${first.subAdminArea}");//Provincia
+                                print("SubLocality: ${first.subLocality}"); //Zona
+                                print("SubThoroughfare: ${first.subThoroughfare}");
+                                print("Thoroughfare: ${first.thoroughfare}");
+                              });
+                            },
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Zona: ${zona}"),
+                              Text("Munic.: ${municipio}"),
+                              Text("Ciudad: ${ciudad}"),
+                            ],
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancelar',style: TextStyle(color: Colors.white, fontSize: 15))
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Continuar',style: TextStyle(color: Colors.white, fontSize: 15))
+                  ),
+                ],
+              )
+            ],
+          );
+        }
     );
   }
 }
