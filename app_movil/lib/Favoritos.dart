07841bc78660a94fb78bb207976c4a47e1cdcf72
Natisita165 <natisita6@gmail.com>
2022-11-17 09:02:03 -0400

    Provider en Favoritos sin completar

--- app_movil/lib/Favoritos.dart
+++ app_movil/lib/Favoritos.dart
@@ -1,7 +1,10 @@
+import 'package:app_movil/servers/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
 
 import 'ClienteServicios.dart';
+import 'DTO/Rating.dart';
 import 'MenuLateral.dart';
 
 
@@ -28,6 +31,49 @@ class RandWordsState extends State<RandWords> {
   final _randomWordPairs = <WordPair>[];
   final _addWordPairs = Set<WordPair>();
 
+  Future<List<Rating>> myRating;
+
+/*
+  Widget anadirFav(){
+    return FutureBuilder(
+      future: myRating,
+      builder: (context, snapshot){
+        if(snapshot.hasData){
+          return _buildList(snapshot.data);
+        }
+        else if (snapshot.data == null){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }*/
+  /*
+  Widget _buildList(List<Rating> myRating) {
+
+    return ListView.builder(
+
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+
+        if (item.isEven) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }*/
+
+
   Widget _buildList() {
     return ListView.builder(
 
@@ -47,9 +93,9 @@ class RandWordsState extends State<RandWords> {
     );
   }
 
+
   Widget _buildRow(WordPair pair) {
     final alreadyadd = _addWordPairs.contains(pair);
-
 
     return ListTile(
         title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
@@ -72,7 +118,7 @@ class RandWordsState extends State<RandWords> {
       return ListTile(
           title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
     });
-
+    myRating = Provider.of<BoActiveProvider>(context, listen: false).getRanting();
     final List<Widget> divided =
     ListTile.divideTiles(context: context, tiles: tiles).toList();
 
